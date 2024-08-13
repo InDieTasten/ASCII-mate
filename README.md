3329313fdc149a3bdc32d14266a90f58f0e77966
InDieTasten <indietasten@gmail.com>
2024-08-13 20:38:18 +0200

    Add raw terminal handling

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ASCII-mate
 A CLI tool with mouse support for drawing ascii art and animations
+
+## Prerequisites
+- /bin/stty
+- /bin/tput
---
+++ main.lua
@@ -0,0 +1,34 @@
+function enterRawMode()
+    os.execute("/bin/stty raw")
+    os.execute("/bin/stty -echo")
+    os.execute("/bin/tput smcup")
+end
+function leaveRawMode()
+    os.execute("/bin/tput rmcup")
+    os.execute("/bin/stty echo")
+    os.execute("/bin/stty sane")
+end
+
+enterRawMode()
+function main()
+    while true do
+        local key = io.read(1)
+        if key == "q" then
+            break
+        end
+        print("Pressed key: " .. key.."\r")
+    end
+end
+
+local status = xpcall(main, function (err)
+    leaveRawMode()
+    traceback = debug.traceback()
+    io.stderr:write(err.."\n"..traceback.."\n")
+    if os.getenv("DEBUG") == "1" then
+        debug.debug()
+    end
+end)
+
+if status then
+    leaveRawMode()
+end
